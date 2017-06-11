d62fb07e5024e8b2e42e3399587027d7c08a6d10
Edilson Borges <edilsonfb@gmail.com>
2017-06-11 13:29:31 -0300

    Highlight variables that need to be changed

--- p5-regex4good/functions/sketch-split.js
+++ p5-regex4good/functions/sketch-split.js
@@ -1,20 +1,18 @@
 function setup() {
-  noCanvas();
-
   input = select('#textinput');
   button = select('#submit');
-  button.mousePressed(newText);
-
+  newText()
+  input.input(newText)
 }
 
 function newText() {
   var s = input.value()
-  var regex = /(\W+)/g;
+  var regex = /\s/g;
   var words = s.split(regex)
   for(i = 0;i < words.length;i++){
     var span = createSpan(words[i])
     span.parent(output)
-    if(/(\w+)/.test(words[i])){
+    if(/([$]\w+)[_]([\w])([\w]+)/g.test(words[i])){
       span.style('background-color','rgb(255, 207, 255)')
     }
   }
