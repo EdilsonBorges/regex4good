505559d827058f82f79190be7110da73739aa1a8
Edilson Borges <edilsonfb@gmail.com>
2017-06-11 20:27:53 -0300

    Replacing content with spaces and in the same parented element

--- p5-regex4good/functions/sketch-split.js
+++ p5-regex4good/functions/sketch-split.js
@@ -1,4 +1,5 @@
 function setup() {
+  noCanvas()
   input = select('#textinput');
   button = select('#submit');
   newText()
@@ -6,14 +7,21 @@ function setup() {
 }
 
 function newText() {
+  clearP()
   var s = input.value()
   var regex = /\s/g;
   var words = s.split(regex)
   for(i = 0;i < words.length;i++){
-    var span = createSpan(words[i])
-    span.parent(output)
+    var span = createSpan(words[i]+' ')
     if(/([$]\w+)[_]([\w])([\w]+)/g.test(words[i])){
       span.style('background-color','rgb(255, 207, 255)')
     }
+    span.parent(output)
   }
+}
+
+function clearP(){
+  outputDiv = select('#output');
+  outputDiv.remove()
+  createP('').id('output')
 }
