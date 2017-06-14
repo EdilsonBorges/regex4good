b03eba64065ff2c564b0948d2c46e1d849599973
Edilson Borges <edilsonfb@gmail.com>
2017-06-13 23:30:59 -0300

    Function matchVariables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,18 +9,21 @@ function setup() {
 function newText() {
   clearP()
   var inputValue = input.value().replace(/\n/g, '<br>').replace(/\s/g, '&nbsp;');
-  var regexVariables = /([$]\w+)[_]([\w])([\w]+)?/g;
   var regexArray = /([, \(])?array\(([a-zA-Z0-9\-\=\,\n\.\ \/\[\]\>\'\"\$\_\*]*)\)/g;
-  var wordsReplace = inputValue.replace(regexVariables, function(){
+  var wordsReplace = inputValue.replace(regexArray, function(){
+    var argument1 = arguments[1] ? arguments[1].toLowerCase() : '';
+     return '<span style=\'background-color:rgb(255, 207, 255); cursor:pointer\'>'+ argument1 + '[' + arguments[2] + ']</span>';
+  });
+  return createP(matchVariables(wordsReplace)).parent(output)  
+}
+
+function matchVariables(value){
+  var regexVariables = /([$]\w+)[_]([\w])([\w]+)?/g;
+  return value.replace(regexVariables, function(){
     var argument3 = arguments[3] ? arguments[3].toLowerCase() : '';
     var matchedWords = arguments[1].toLowerCase() + arguments[2].toUpperCase() + argument3;
     return '<span style=\'background-color:rgb(255, 207, 255); cursor:pointer\'>'+matchedWords+'</span>'
   });
-  var wordsReplace = wordsReplace.replace(regexArray, function(){
-    var argument1 = arguments[1] ? arguments[1].toLowerCase() : '';
-     return '<span style=\'background-color:rgb(255, 207, 255); cursor:pointer\'>'+ argument1 + '[' + arguments[2] + ']</span>';
-  });
-  return createP(wordsReplace).parent(output)  
 }
 
 function clearP(){
