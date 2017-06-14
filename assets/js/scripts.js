344c7905b43c307c22b4a7fc14847ee4f05baea5
Edilson Borges <edilsonfb@gmail.com>
2017-06-13 22:29:18 -0300

    Functional regex replace with span

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,37 +9,15 @@ function setup() {
 function newText() {
   clearP()
   var inputValue = input.value();
-  var regexSpace = /\s/g;
   var regexVariables = /([$]\w+)[_]([\w])([\w]+)?/g;
   var regexArray = /([, \(])array\(([a-zA-Z0-9\-\=\,\n\.\ \/\[\]\>\'\"\$\_\*]*)\)/g;
-  var words = inputValue.split(regexSpace);
-  var wordsReplace = inputValue.replace(regexVariables, function(r){
-    console.log(arguments)
+  var wordsReplace = inputValue.replace(regexVariables, function(){
     var argument3 = arguments[3] ? arguments[3].toLowerCase() : '';
-    return arguments[1].toLowerCase() + arguments[2].toUpperCase() + argument3;
+    var matchedWords = arguments[1].toLowerCase() + arguments[2].toUpperCase() + argument3;
+    return '<span style=\'background-color:rgb(255, 207, 255)\'>'+matchedWords+'</span>'
   });
-  console.log(wordsReplace)
-  for (i = 0;i < words.length;i++) {
-    if (regexVariables.test(words[i])) {
-      var replacedMatchVariables = words[i].replace(regexVariables, function(){
-        var argument3 = arguments[3] ? arguments[3].toLowerCase() : '';
-        return arguments[1].toLowerCase() + arguments[2].toUpperCase() + argument3;
-      })
-      var span = createSpan(replacedMatchVariables + ' ')
-      span.style('background-color','rgb(255, 207, 255)')
-    } else if (regexArray.test(words[i])) {
-      var replacedMatchArray = words[i].replace(regexArray, function(){
-        console.log(arguments)
-        return arguments[1] + '[' + arguments[2] + ']';
-      })
-      var span = createSpan(replacedMatchArray + ' ')
-      span.style('background-color','rgb(255, 207, 255)')
-    } else {
-      var span = createSpan(words[i] + ' ')
-
-    }
-    span.parent(output)
-  }
+    var p = createP(wordsReplace)
+    return p.parent(output)
 }
 
 function clearP(){
